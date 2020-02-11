8fb19538dd1b34cc50a7f8ba08579a127dad520c
Pranav Rajan <rajan.pranav320@gmail.com>
2020-02-10 21:51:12 -0700

    started the set up for the MIPS program

---
+++ HW_4_RAJAN_PRANAV/data_compression.asm
@@ -0,0 +1,29 @@
+# Data Compression
+# Name: Pranav Rajan
+# uID: U1136324
+# umail: u1136324@utah.edu
+
+# The program accepts an input string of size less than 40 characters
+# and applies run length encoding compression to the string and prints
+# the resulting compressed string. The input string only consists of 
+# alphabets ie. a-z and A-Z
+
+# The following sources were consulted and used:
+# 1) Professor Rajeev Balasubramonian Skeleton
+# http://www.cs.utah.edu/~rajeev/cs3810/hw/skel.s.txt
+# 2) Appendix A from P&H 5th edition
+# 3) Professor Rajeev Balasubramonian MARS Notes
+# https://docs.google.com/document/d/15m7ypUuAErcpfcLTzv-efMIfjlYZPH8YkqUTa3oJkHw/edit
+# 4) Professor Rajeev Balasubramonian Class Lectures
+
+.data # the data for the program
+.align 0 # byte-align the 45 byte space declared next
+buffer: .space 45 # allocate 45 bytes as a read buffer for string input
+
+
+
+
+              
+              
+              
+                        
